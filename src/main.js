5e1bd84c3a87be559d24fabd392d5c168ff6893d
杨小在 <y.jinbiao@foxmail.com>
2021-09-18 11:39:51 +0800

    引用ant-design-vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// 引用ant
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
+
 Vue.config.productionTip = false;
 
 new Vue({
