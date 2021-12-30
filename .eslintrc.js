74c28a9249c513b74df7f5050035312db891cb76
杨小在 <y.jinbiao@foxmail.com>
2021-12-30 09:57:43 +0800

    feat(map模块): ✨ 新增高德地图demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,14 @@ module.exports = {
     'no-multi-assign': 'off', // 禁止连续赋值 关闭
     'no-shadow': 'off', // 禁止变量声明与外层作用域的变量同名
     'no-param-reassign': 'off', 	// 禁止对 function 的参数进行重新赋值
-    'dot-notation': 'off', // 强制尽可能地使用点号
+    'dot-notation': 'off', // 强制尽可能地使用点号,
+    'new-cap': 'off',
+    'no-restricted-globals': 'off',
   },
   globals: {
     // 高德地图
     AMap: true,
     Loca: true,
+    map: true,
   },
 };
