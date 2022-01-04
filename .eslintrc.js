61b7ba52b1aa45bf62a29bdcece5ffc4bd47b758
杨小在 <y.jinbiao@foxmail.com>
2022-01-04 10:28:27 +0800

    ✨ feat(global): 使用AlloyTeam的规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,26 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es6: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
-  // plugins: ['prettier'],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+  extends: [
+    'alloy',
+    'alloy/vue',
+  ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'prettier/prettier': 'error',
-    'no-tabs': 'off', // 禁用tab 关闭
-    'no-multi-assign': 'off', // 禁止连续赋值 关闭
-    'no-shadow': 'off', // 禁止变量声明与外层作用域的变量同名
-    'no-param-reassign': 'off', 	// 禁止对 function 的参数进行重新赋值
-    'dot-notation': 'off', // 强制尽可能地使用点号,
-    'new-cap': 'off',
-    'no-restricted-globals': 'off',
-    'arrow-parens': 'off',
-    'object-curly-newline': 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // // 'prettier/prettier': 'error',
+    // 'no-tabs': 'off', // 禁用tab 关闭
+    // 'no-multi-assign': 'off', // 禁止连续赋值 关闭
+    // 'no-shadow': 'off', // 禁止变量声明与外层作用域的变量同名
+    // 'no-param-reassign': 'off', 	// 禁止对 function 的参数进行重新赋值
+    // 'dot-notation': 'off', // 强制尽可能地使用点号,
+    // 'new-cap': 'off',
+    // 'no-restricted-globals': 'off',
+    // 'arrow-parens': 'off',
+    // 'object-curly-newline': 'off',
   },
   globals: {
     // 高德地图
