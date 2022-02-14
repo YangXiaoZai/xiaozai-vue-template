b920316d9c5fb8f7ed4d867cbe7b8687caad74aa
杨小在 <y.jinbiao@foxmail.com>
2022-02-14 09:33:54 +0800

    ✨ feat(utils): 新增获取随机数的公共方法

--- src/utils/num.js
+++ src/utils/num.js
@@ -36,3 +36,31 @@ export const splitFloatFontSize = (num, f1 = 16, f2 = 14) => {
   const splitNum = formatFloat(num).split('.');
   return `<span style='font-size:${f1}px;line-heigh:1'>${splitNum[0]}.</span><span style='font-size:${f2}px;line-heigh:1}'>${splitNum[1]}</span>`;
 };
+
+/**
+ * 随机生成整数型数字(默认1~100)
+ *  return => 23
+ * @param {number} n // 最小范围的值
+ * @param {number} m // 最大范围的值
+ *
+ * @return {number} num // 返回值 (n <= num <= m)
+ *
+ */
+export const getRandomInt = (n = 1, m = 100) => {
+  let num = Math.floor(Math.random() * (m - n + 1) + n);
+  return num;
+};
+
+/**
+ * 随机生成浮点型数字(默认1~100)
+ * return => 42.31
+ * @param {number} n // 最小范围的值
+ * @param {number} m // 最大范围的值
+ *
+ * @return {number} num // 返回值 (n <= num <= m)
+ *
+ */
+export const getRandomFloat = (n = 1, m = 100, point = 2) => {
+  let num = (Math.random() * (m - n) + n).toFixed(point);
+  return num;
+};
