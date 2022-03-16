a3aa33e8bce15e5302ee6482fe5c3b06b11ff7fc
杨小在 <y.jinbiao@foxmail.com>
2022-03-16 10:04:02 +0800

    ✨ feat(echart图表组件的使用): 组件应用

---
+++ src/views/dashboard/components/LineChart.vue
@@ -0,0 +1,41 @@
+<!-- 折线图 -->
+<template>
+  <BaseChart :type="type" :chart-data="chartData" :title="title"></BaseChart>
+</template>
+
+<script>
+import BaseChart from '@/components/ECharts/BaseChart';
+export default {
+  components: { BaseChart },
+  data() {
+    return {
+      type: 'line',
+      title: '各产品每日销售量折线图',
+      chartData: [
+        { name: '家用电器', data: [120, 210, 220, 322, 425, 533, 573] },
+        { name: '食用酒水', data: [79, 51, 122, 132, 145, 223, 299] },
+        { name: '个护健康', data: [123, 142, 242, 232, 245, 276, 233] },
+        { name: '服饰箱包', data: [212, 321, 344, 356, 378, 399, 423] },
+        { name: '母婴产品', data: [42, 21, 22, 32, 45, 43, 38] },
+      ],
+    };
+  },
+  computed: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+.container {
+}
+</style>
---
+++ src/views/dashboard/components/RadarChart.vue
@@ -0,0 +1,54 @@
+<!-- 雷达图 -->
+<template>
+  <RadarChart :title="title" :chart-data="chartData" :radar="radar"></RadarChart>
+</template>
+
+<script>
+import RadarChart from '@/components/ECharts/RadarChart';
+export default {
+  components: { RadarChart },
+  data() {
+    return {
+      title: '各大区综合对比图',
+      chartData: [
+        {
+          value: [4200, 3000, 20000, 35000, 50000],
+          name: '订单量',
+        },
+        {
+          value: [5000, 14000, 28000, 26000, 42000],
+          name: '销售额',
+        },
+      ],
+      radar: {
+        //  形状圆
+        // shape: 'circle',
+        indicator: [
+          { name: '华东大区', max: 6500 },
+          { name: '华西大区', max: 16000 },
+          { name: '华南大区', max: 30000 },
+          { name: '华北大区', max: 38000 },
+          { name: '华中大区', max: 52000 },
+        ],
+      },
+    };
+  },
+  computed: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+.container {
+}
+</style>
