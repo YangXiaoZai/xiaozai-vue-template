447507a6884b8223b2c8e568bb7853dab02c6fc6
杨小在 <y.jinbiao@foxmail.com>
2022-05-05 14:51:07 +0800

    ✨ feat(顶部菜单栏): 新增全屏

--- src/layout/components/UserMenu/index.vue
+++ src/layout/components/UserMenu/index.vue
@@ -1,19 +1,31 @@
 <!-- 用户菜单 -->
 <template>
-  <a-dropdown>
-    <div class="user-info">
-      <img v-if="userInfo.avatar" :src="fileBaseUrl + userInfo.avatar" alt="用户头像" class="user-avatar" />
-      <a-icon v-else type="user" class="user-avatar" />
-      <span>{{ userInfo.nickname || '昵称' }}</span>
-    </div>
-    <a-menu slot="overlay">
-      <a-menu-item> <router-link to="/user">个人中心 </router-link></a-menu-item>
-      <a-menu-item>
-        <a href="https://github.com/YangXiaoZai/xiaozai-vue-template" target="_blank">仓库地址 </a>
-      </a-menu-item>
-      <a-menu-item @click="logout"> 退出登录 </a-menu-item>
-    </a-menu>
-  </a-dropdown>
+  <div class="flex-center">
+    <a-space>
+      <div @click="handleFullscreen">
+        <a-tooltip>
+          <template slot="title"> {{ fullscreen ? '退出全屏' : '全屏' }} </template>
+          <a-icon v-if="fullscreen" class="full-icon" type="fullscreen-exit" />
+          <a-icon v-else class="full-icon" type="fullscreen" />
+        </a-tooltip>
+      </div>
+
+      <a-dropdown>
+        <div class="user-info">
+          <img v-if="userInfo.avatar" :src="fileBaseUrl + userInfo.avatar" alt="用户头像" class="user-avatar" />
+          <a-icon v-else type="user" class="user-avatar" />
+          <span>{{ userInfo.nickname || '昵称' }}</span>
+        </div>
+        <a-menu slot="overlay">
+          <a-menu-item> <router-link to="/user">个人中心 </router-link></a-menu-item>
+          <a-menu-item>
+            <a href="https://github.com/YangXiaoZai/xiaozai-vue-template" target="_blank">仓库地址 </a>
+          </a-menu-item>
+          <a-menu-item @click="logout"> 退出登录 </a-menu-item>
+        </a-menu>
+      </a-dropdown>
+    </a-space>
+  </div>
 </template>
 
 <script>
@@ -21,6 +33,9 @@ import { mapGetters } from 'vuex';
 export default {
   data() {
     return {
+      // 全屏
+      fullscreen: false,
+
       fileBaseUrl: process.env.VUE_APP_FILE_BASE_URL,
     };
   },
@@ -32,11 +47,44 @@ export default {
       await this.$store.dispatch('user/logout');
       this.$router.push('/login');
     },
+    handleFullscreen() {
+      let element = document.documentElement;
+      if (this.fullscreen) {
+        // 退出全屏
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      } else {
+        // 全屏
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.webkitRequestFullScreen) {
+          element.webkitRequestFullScreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.msRequestFullscreen) {
+          // IE11
+          element.msRequestFullscreen();
+        }
+      }
+      this.fullscreen = !this.fullscreen;
+    },
   },
 };
 </script>
 <style lang="less" scoped>
 .container {
+  .full-icon {
+    font-size: 20px;
+    cursor: pointer;
+    vertical-align: sub;
+  }
   .user-info {
     display: flex;
     justify-content: center;
