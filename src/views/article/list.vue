40045e220b8380e153a49538a208c65d462241b8
杨小在 <y.jinbiao@foxmail.com>
2022-05-05 11:27:41 +0800

    ✨ feat(vue-infinite-scroll): 新增文章页面滚动加载demo

--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -1,5 +1,5 @@
 <!-- 列表页 -->
-<!-- TODO 增加滚动加载 -->
+<!-- TODO 此页面为demo，实际项目中需要考虑交互 -->
 <template>
   <div class="container">
     <div class="search-container flex-between">
@@ -18,33 +18,45 @@
         <a-form-item>
           <a-space>
             <a-button @click="reset">重置</a-button>
-            <a-button type="primary" @click="getArticle">搜索</a-button>
+            <a-button type="primary" @click="search">搜索</a-button>
           </a-space>
         </a-form-item>
       </a-form>
       <a-button icon="plus" type="primary" @click="$router.push('/article/create')">创建文章</a-button>
     </div>
-    <div class="list-container">
-      <a-row :gutter="[40, 24]">
-        <a-col v-for="(item, index) in listData" :key="index" :xs="24" :sm="12" :md="8" :lg="6" :xl="4">
-          <a-skeleton active :loading="skeletonLoading">
-            <a-card hoverable @click="$router.push(`/article/preview/${item.id}`)">
-              <img slot="cover" alt="封面" :src="fileBaseUrl + item.cover" />
-              <a-card-meta :title="item.title">
-                <template slot="description">{{ item.createAt }}</template>
-              </a-card-meta>
-            </a-card>
-          </a-skeleton>
-        </a-col>
-      </a-row>
-    </div>
+
+    <a-spin :spinning="loading && !busy">
+      <div class="container-shadow">
+        <div
+          v-show="listData.length > 0"
+          v-infinite-scroll="loadMore"
+          :infinite-scroll-disabled="busy"
+          :infinite-scroll-distance="10"
+          class="list-container"
+        >
+          <a-row :gutter="[40, 24]" class="row-container">
+            <a-col v-for="(item, index) in listData" :key="index" :xs="24" :sm="12" :md="8" :lg="6" :xl="4">
+              <a-card hoverable @click="$router.push(`/article/preview/${item.id}`)">
+                <img slot="cover" alt="封面" :src="fileBaseUrl + item.cover" />
+                <a-card-meta :title="item.title">
+                  <template slot="description">{{ item.createAt }}</template>
+                </a-card-meta>
+              </a-card>
+            </a-col>
+          </a-row>
+        </div>
+        <a-empty v-show="listData.length == 0"></a-empty>
+      </div>
+    </a-spin>
   </div>
 </template>
 
 <script>
+import infiniteScroll from 'vue-infinite-scroll';
 import { getArticleType, getArticle } from '@/api/article';
 export default {
   components: {},
+  directives: { infiniteScroll },
   data() {
     return {
       articleType: [],
@@ -52,53 +64,80 @@ export default {
         title: null,
         type: undefined,
       },
-      listData: null,
-      skeletonLoading: true,
+      pagination: {
+        current: 1,
+        pageSize: 24,
+      },
       fileBaseUrl: process.env.VUE_APP_FILE_BASE_URL,
+      listData: [],
+      busy: false,
+      loading: false,
     };
   },
-  computed: {},
-  // 生命周期 - 创建完成（可以访问当前this实例）
   created() {
     this.getArticleType();
-    this.getArticle();
   },
-  // 生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, // 生命周期 - 创建之前
-  beforeMount() {}, // 生命周期 - 挂载之前
-  beforeUpdate() {}, // 生命周期 - 更新之前
-  updated() {}, // 生命周期 - 更新之后
-  beforeDestroy() {}, // 生命周期 - 销毁之前
-  destroyed() {}, // 生命周期 - 销毁完成
-  activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
   methods: {
     async getArticleType() {
       const { data } = await getArticleType();
       this.articleType = data;
     },
     async getArticle() {
-      this.skeletonLoading = true;
-      const params = this.form;
+      const params = {
+        ...this.pagination,
+        ...this.form,
+      };
       const { data } = await getArticle(params);
-
-      console.log(data);
-      this.listData = data.rows;
-      this.skeletonLoading = false;
+      if (data.rows.length == 0) {
+        this.$message.info('已经没有数据了');
+        this.busy = true;
+        this.loading = false;
+        return;
+      }
+      this.listData = this.listData.concat(data.rows);
+      this.loading = false;
     },
     reset() {
-      // TODO优化
       this.form.title = null;
       this.form.type = undefined;
+      this.search();
+    },
+    search() {
+      this.pagination.current = 1;
+      this.pagination.pageSize = 24;
+      this.listData = [];
       this.getArticle();
+      this.pagination.current++;
+    },
+    async loadMore() {
+      this.loading = true;
+      await this.getArticle();
+      this.pagination.current++;
     },
   },
 };
 </script>
 <style lang="less" scoped>
 .container {
-  .list-container {
+  .container-shadow {
     margin-top: 24px;
+    padding: 16px;
+    box-sizing: border-box;
+    box-shadow: 0 8px 20px rgba(143, 168, 191, 0.35);
+    border-radius: 6px;
+    cursor: pointer;
+    transition: 0.5s;
+    min-height: 500px;
+    &:hover {
+      box-shadow: 0 8px 20px rgba(143, 168, 191, 0.65);
+    }
+    .list-container {
+      overflow: auto;
+      overflow-x: hidden;
+      .row-container {
+        overflow: auto;
+      }
+    }
   }
 }
 </style>
